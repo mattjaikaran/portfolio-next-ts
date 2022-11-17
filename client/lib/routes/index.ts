13c3aae34e3dec15a032ae291827a578d595860a
Matt Jaikaran <mattjaikaran@gmail.com>
2022-11-17 18:08:48 -0500

    wip

--- client/lib/routes/index.ts
+++ client/lib/routes/index.ts
@@ -2,7 +2,7 @@ import { IconHome2, IconAddressBook, IconCodeCircle2 } from '@tabler/icons';
 
 export const routes = [
   { link: '/', label: 'Home', icon: IconHome2 },
-  // { link: '/about', label: 'About', icon: MdBookmarkBorder },
+  { link: '/about', label: 'About', icon: IconHome2 },
   { link: '/portfolio', label: 'Portfolio', icon: IconCodeCircle2 },
   { link: '/contact', label: 'Contact', icon: IconAddressBook },
 ];
