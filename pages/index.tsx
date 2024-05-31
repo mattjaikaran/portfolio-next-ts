9568b49652c30f0cb00aab3cad1e494fbc57eca7
Matthew Jaikaran <mattjaikaran@gmail.com>
2024-05-31 00:38:44 -0400

    update home page, removed corrupted photos, added photo npm library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 import { HeadingH1, HeadingH3, Paragraph } from '@/components/typography';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { siteConfig } from '@/config/site';
 import { MainLayout } from '@/layouts/MainLayout';
 import { useRouter } from 'next/router';
-import birds from '@/assets/images/birds.jpg';
-import pages from '@/assets/images/jj-ying-WmnsGyaFnCQ-unsplash.jpg';
-import texture1 from '@/assets/images/pawel-czerwinski-HbcfaO4m03s-unsplash.jpg';
-import hubble from '@/assets/images/hubble.svg';
-import hyvee from '@/assets/images/hyvee.png';
-import avon from '@/assets/images/avon.png';
-import gm from '@/assets/images/gm.png';
-import rewyre from '@/assets/images/rewyre-logo.svg';
-import spotify from '@/assets/images/spotify-green.png';
+import birds from '@/public/images/birds.jpg';
+import pages from '@/public/images/jj-ying-WmnsGyaFnCQ-unsplash.jpg';
+import hubble from '@/public/images/hubble.svg';
+import hyvee from '@/public/images/hyvee.png';
+import avon from '@/public/images/avon.png';
+import gm from '@/public/images/gm.png';
+import rewyre from '@/public/images/rewyre-logo.svg';
+import spotify from '@/public/images/spotify-green.png';
+
+import grandCentral from '@/public/images/photos/grand-central-rain.jpg';
+import unionSquareCouple from '@/public/images/photos/unionsquare-couple.jpg';
+import midtown34th from '@/public/images/photos/midtown34th.jpg';
 
 export default function Home() {
   const router = useRouter();
@@ -33,6 +35,7 @@ export default function Home() {
             View Portfolio
           </Button>
         </div>
+
         {/* <div className="p-8 md:p-24 lg:p-32 bg-foreground text-background md:px-64 md:py-32 text-center">
           <HeadingH3>Title</HeadingH3>
           <Paragraph>
@@ -44,14 +47,20 @@ export default function Home() {
             See More
           </Button>
         </div> */}
+
         <img src={pages.src} alt="img" className="sm:hidden" />
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <img src={grandCentral.src} alt="img" className="sm:hidden" />
+          <img src={unionSquareCouple.src} alt="img" className="sm:hidden" />
+        </div>
+
         {/* <div className="bg-foreground text-background p-24 text-center">
           <HeadingH3>what up</HeadingH3>
           <Button variant="outline" className="bg-foreground text-background">
             Click Here
           </Button>
         </div> */}
-        {/* <img className="sm:hidden" src={texture1.src} alt="img" /> */}
+
         <div className="p-16 md:p-32 bg-white border-t-2 text-center">
           <HeadingH3 className="text-black">{`Brands I've worked with`}</HeadingH3>
           <div className="mt-12 lg:ml-32 grid grid-cols-1 md:grid-cols-2 gap-8 justify-center">
@@ -95,30 +104,27 @@ export default function Home() {
             </a>
           </div>
         </div>
+
         {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2">
           <div className="p-24 text-center">
-            <HeadingH3>Some Title</HeadingH3>
-            <Paragraph>here is a one liner</Paragraph>
-            <Button variant="outline" onClick={() => router.push('/portfolio')}>
-              View Details
+            <HeadingH3>Music</HeadingH3>
+            <Button variant="outline" onClick={() => router.push('/music')}>
+              View Music
             </Button>
           </div>
-          <div className="p-24 bg-foreground text-center">
-            <HeadingH3 className="text-background">hell yeah</HeadingH3>
-            <Paragraph className="text-background">
-              this isnt done yet so ur cool if ur seeing this
-            </Paragraph>
+          <div className="p-24 text-center">
+            <HeadingH3>Photos</HeadingH3>
             <Button
               variant="outline"
-              className="bg-foreground text-background"
-              onClick={() => router.push('/portfolio')}
+              onClick={() => router.push('/photography')}
             >
-              View Details
+              View Photos
             </Button>
           </div>
-        </div>
-         */}
+        </div> */}
+
         <img src={birds.src} alt="img" className="sm:hidden" />
+        {/* <img src={midtown34th.src} alt="img" className="sm:hidden" /> */}
         {/* newsletter */}
         <div className="p-16 md:px-64 md:py-32 text-center border-t-2">
           <HeadingH3>Want to collaborate on a project?</HeadingH3>
