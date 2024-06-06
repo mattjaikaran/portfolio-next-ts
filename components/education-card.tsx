c7c462a6b7dd8beee0f6ed4d74f86374f97c9a99
Matthew Jaikaran <mattjaikaran@gmail.com>
2024-06-06 00:14:04 -0400

    refactored about cards into new components

---
+++ components/education-card.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { educationData } from '@/data/portfolio';
+import { Paragraph } from './typography';
+
+export default function EducationCard() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Education</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {educationData.map((edu, index) => (
+          <div key={index} className="mb-4">
+            <Paragraph>
+              <strong>{edu.title}</strong>
+              {edu.location ? (
+                <>
+                  <br />
+                  <small>{edu.location}</small>
+                </>
+              ) : null}
+              <br />
+              {edu.description}
+              <br />
+              {edu.type}
+            </Paragraph>
+          </div>
+        ))}
+      </CardContent>
+    </Card>
+  );
+}
